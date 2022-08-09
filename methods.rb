cb23dade9f67a86296ec61f93f8734eeec1238fd
Ash Dan <Arvolley01@gmail.com>
2022-08-09 10:56:24 -0400

    commit tree 308001833f8964e1e24427ec38d285b87e3ea5cb

--- methods.rb
+++ methods.rb
@@ -1,1 +1,21 @@
 # Your code here!
+
+def greet_programmer
+    puts 'Hello, programmer!'
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = 'programmer')
+    greet(name)
+end
+
+def add (x, y)
+    x + y 
+end
+
+def halve (a)
+    a.class != Integer ? return : a / 2
+end
